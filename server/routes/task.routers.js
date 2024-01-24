04b0abb43e81e632967e41dd5b51247bf5596828
Azachirst <108694709+Azachirst@users.noreply.github.com>
2024-01-24 05:14:18 -0300

    Update task.routers.js

--- server/routes/task.routers.js
+++ server/routes/task.routers.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import{getTasks, getTask, createTask, updateTask, deleteTask, getHorarios, getHorario, createHorario, updateHorario, deleteHorario, getMoviles, getMovil, createMovil, updateMovil, deleteMovil} from '../controllers/task.controllers.js'
+import{getTasks, getTask, createTask, updateTask, deleteTask, getMoviles, getMovil, createMovil, updateMovil, deleteMovil} from '../controllers/task.controllers.js'
 
 const router = Router();
 
@@ -14,18 +14,6 @@ router.post('/secuencia', createTask)
 router.put('/secuencia/:id', updateTask)
 
 router.delete('/secuencia/:id', deleteTask)
-
-//horarios
-
-router.get('/horarios', getHorarios)
-
-router.get('/horario/:id', getHorario)
-
-router.post('/horario', createHorario)
-
-router.put('/horario/:id', updateHorario)
-
-router.delete('/horario/:id', deleteHorario)
 
 //moviles
 
